b5131f51cb1d6fcfef151c46c5257b70dd4ab540
James Alan Bush <jamesalanbush1972@gmail.com>
2023-08-29 10:55:39 -0400

    Xcode Cloud distribution

--- AVCamManual/AVCamManualCameraViewController.h
+++ AVCamManual/AVCamManualCameraViewController.h
@@ -1,10 +1,10 @@
 /*
-	Copyright (C) 2016 Apple Inc. All Rights Reserved.
-	See LICENSE.txt for this sample’s licensing information
-	
-	Abstract:
-	View controller for camera interface.
-*/
+ Copyright (C) 2016 Apple Inc. All Rights Reserved.
+ See LICENSE.txt for this sample’s licensing information
+ 
+ Abstract:
+ View controller for camera interface.
+ */
 
 @import UIKit;
 
